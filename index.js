a6cdceae870cce0893c604e583fdb13c1db8008d
CDD <70897880+CDDNB@users.noreply.github.com>
2022-03-06 17:25:13 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var map = new BMapGL.Map("container");
+var map = new BMapGL.Map("modern");
 var point = new BMapGL.Point(118.78, 32.06);
 map.centerAndZoom(point, 14); // 初始化地图，设置中心点坐标和地图级别 
 map.setMapStyleV2({ styleId: 'e7c8f51a84922c56ac0884f7b85b2356' });
@@ -11,7 +11,7 @@ map.setTilt(45);
     //building: false  // 隐藏楼块
 });*/
 
-var myIcon = new BMapGL.Icon("images/lantern2.png", new BMapGL.Size(32, 32));
+var myIcon = new BMapGL.Icon("img/lantern.png", new BMapGL.Size(32, 32));
 
 // 创建Marker标注，使用孔明灯图标
 
@@ -26,3 +26,27 @@ point[5] = new BMapGL.Point(118.775830, 32.059739);
 point[6] = new BMapGL.Point(118.784569, 32.049938);
 point[7] = new BMapGL.Point(118.782470, 32.071342);
 point[8] = new BMapGL.Point(118.764077, 32.086597);
+
+function func() {
+    t = document.getElementById("test");//.style.display = 'none';
+    if (t.style.display == 'none') {
+        document.getElementById("container").style.display = 'none';
+        t.style.display = 'block';
+    }
+    else {
+        t.style.display = 'none';
+        document.getElementById("container").style.display = 'block';
+    }
+}
+
+function func() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if(this.readyState == 4 && this.status == 200) {
+            console.log(this.responseText);
+        }
+    };
+    xhttp.open("GET", "1.txt", true);
+    xhttp.send();
+};func();
+
