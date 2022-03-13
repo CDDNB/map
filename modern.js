6cb26e7bdd8c66288e02f348a56b8b597a9dfa4d
CDD <70897880+CDDNB@users.noreply.github.com>
2022-03-14 05:43:29 +0800

    Add files via upload

--- modern.js
+++ modern.js
@@ -1,11 +1,17 @@
-document.getElementById("history").style.display = 'none';
-var certificate = document.getElementById("certificate");
-certificate.style.display = 'none';
-certificate.addEventListener("click", function() {
+document.getElementById("modern").style.display = 'none';
+document.getElementById("gif").style.display = 'none';
+document.getElementById("canvas").style.display = 'none';
+var question = document.getElementById("question");
+question.style.display = 'none';
+var introduction = document.getElementById("introduction");
+introduction.style.display = 'none';
+var picture = document.getElementById("pic");
+picture.style.display = 'none';
+picture.addEventListener("click", function() {
     this.style.display = 'none';
 })
-
-//document.getElementById("button2").style.backgroundImage = "url('img/button1.jpeg')";//opacity = 0.5;
+var question_text = document.getElementById("font");
+var correct_answer = -1;
 
 //按钮
 function to_modern() {
@@ -13,71 +19,147 @@ function to_modern() {
     document.getElementById("modern").style.display = 'block';
     document.getElementById("button1").style.backgroundImage = "url('img/button11.png')";
     document.getElementById("history").style.display = 'none';
+    if(correct_answer != -1) {
+        question.style.display = 'block';
+    }
 }
 function to_history() {
     document.getElementById("button1").style.backgroundImage = "url('img/button1.png')";
     document.getElementById("history").style.display = 'block';
     document.getElementById("button2").style.backgroundImage = "url('img/button22.png')";
     document.getElementById("modern").style.display = 'none';
+    question.style.display = 'none';
+}
+function answer_yes() {
+    document.getElementById("choice1").style.backgroundImage = "url('img/choice_focus.png')";
+    document.getElementById("choice2").style.backgroundImage = "url('img/choice.png')";
+    if (correct_answer == 1) {
+        document.getElementById("choice1").style.backgroundImage = "url('img/choice.png')";
+        answer_correct();
+    }
+}
+function answer_no() {
+    document.getElementById("choice1").style.backgroundImage = "url('img/choice.png')";
+    document.getElementById("choice2").style.backgroundImage = "url('img/choice_focus.png')";
+    if (correct_answer == 0) {
+        document.getElementById("choice2").style.backgroundImage = "url('img/choice.png')";
+        answer_correct();
+    }
+}
+function answer_correct() {
+    correct_answer = -1;
+    setTimeout(function(){
+        question.style.display = 'none';
+        document.getElementById("gif").style.display = 'block';
+        setTimeout(function() {
+            document.getElementById("gif").style.display = 'none';
+            landmarks[state-1].walk(20);
+        }, 4000);
+    },0);
 }
 
 function finish() {
-    certificate.style.display = 'block';
+    var canvas = document.getElementById("canvas");
+    var ctx = canvas.getContext("2d");
+    ctx.font = "40px Georgia";
+    ctx.fillText("Mr. Peace", 0, 0);
+    var img = new Image();
+    img.src = "img/icon_raw.png";
+    img.onload = function() {
+        ctx.drawImage(img, 0, 0);
+        //var a = document.getElementByIdById("a");
+        //a.href = canvas.toDataURL();
+    }
+    canvas.addEventListener("click", function() {
+        canvas.style.display = 'none';
+    });
 }
 
 var landmarks = new Array();
 
 //地图
 var map = new BMapGL.Map("modern");
-var point = new BMapGL.Point(118.764077, 32.086597);//118.78, 32.06);
 //map.addControl(new BMapGL.NavigationControl3D());
-map.centerAndZoom(point, 16); // 初始化地图，设置中心点坐标和地图级别 
-map.setMapStyleV2({ styleId: 'e7c8f51a84922c56ac0884f7b85b2356' });
+//map.centerAndZoom(point, 16); // 初始化地图，设置中心点坐标和地图级别 
+//map.setMapStyleV2({ styleId: 'e7c8f51a84922c56ac0884f7b85b2356' });
 map.enableScrollWheelZoom(true);
 map.setTilt(45);
+map.setMapStyleV2({
+    feathers: 'road point water building'
+});
 
 //地标
-function landmark(index, map_x, map_y, src, img) {
+function landmark(index, name, map_x, map_y, src, pic) {
     this.index = index;
+    this.name = name;
     this.point = new BMapGL.Point(map_x, map_y);
     this.icon = new BMapGL.Icon(src, new BMapGL.Size(45, 45));
     this.marker = new BMapGL.Marker(this.point, {icon : this.icon});
+    this.pic = pic;
     //this.marker = new BMapGL.Marker(this.point, {icon: myIcon});
-    this.img = img;
+    this.questions = [];
+    this.introduction_word = [];
+    this.on_introduction = 1;
+
+    //modern
     this.marker.addEventListener("click", function() {
         this.removeEventListener("click", arguments.callee);
+        question.style.display = 'block';
+        question_text.innerHTML = landmarks[index].questions[0][0];
+        document.getElementById("name").innerHTML = landmarks[index].name;
+        correct_answer = landmarks[index].questions[0][1];
         if (state > index)
             return;
         if(index < 9) {
             state += 1;
             localStorage.setItem("state", state);
-            console.log(state, localStorage.getItem("state"));
+            //console.log(index, state);
             //landmarks[index+1].modern_init();
-            landmarks[index].walk(20);
+            //landmarks[index].walk(20);
         }
-        else {
-            finish();
-        }
+    });
+
+    let id = "l" + index;
+    //old
+    document.getElementById(id).addEventListener("click", function() {
+        landmarks[index].on_introduction = 1;
+        document.getElementById("introduction_name").innerHTML = landmarks[index].name;
+        document.getElementById("introduction_word").innerHTML = landmarks[index].introduction_word[0];
+        introduction.style.display = "block";
+        introduction.addEventListener("click", function() {
+            //console.log(landmarks[index].on_introduction, landmarks[index].name);
+            //alert("?");
+            landmarks[index].on_introduction++;
+            if(landmarks[index].on_introduction > landmarks[index].introduction_word.length) {
+                this.style.display = 'none';
+                introduction.removeEventListener("click", arguments.callee);
+                picture.style.backgroundImage = pic;
+                picture.style.display = 'block';
+            }
+            else {
+                document.getElementById("introduction_word").innerHTML = landmarks[index].introduction_word[landmarks[index].on_introduction-1];
+            }
+        });
     });
 }
 landmark.prototype.modern_init = function() {
     map.addOverlay(this.marker);
 }
 
-landmarks[0] = new landmark(0, 0, 0, 0, 0);
-landmarks[1] = new landmark(1, 118.764077, 32.086597, "img/s1.png", document.getElementById("l1"));
-landmarks[2] = new landmark(2, 118.775927, 32.070405, "img/s2.png", document.getElementById("l2"));
-landmarks[3] = new landmark(3, 118.782470, 32.071342, "img/s3.png", document.getElementById("l3"));
-landmarks[4] = new landmark(4, 118.789756, 32.063179, "img/s4.png", document.getElementById("l4"));
-landmarks[5] = new landmark(5, 118.787487, 32.063273, "img/s5.png", document.getElementById("l5"));
-landmarks[6] = new landmark(6, 118.784773, 32.061284, "img/s6.png", document.getElementById("l6"));
-landmarks[7] = new landmark(7, 118.775830, 32.059739, "img/s7.png", document.getElementById("l7"));
-landmarks[8] = new landmark(8, 118.789863, 32.056754, "img/s8.png", document.getElementById("l8"));
-landmarks[9] = new landmark(9, 118.784569, 32.049938, "img/s9.png", document.getElementById("l9"));
+landmarks[0] = null;
+landmarks[1] = new landmark(1, "国民政府交通部", 118.764077, 32.086597, "img/s1.png", "src/1.jpg");
+landmarks[2] = new landmark(2, "安全区国际委员会总部", 118.775927, 32.070405, "img/s2.png", "src/2.jpeg");
+landmarks[3] = new landmark(3, "华侨招待所", 118.782470, 32.071342, "img/s3.png", "src/3.jpeg");
+landmarks[4] = new landmark(4, "鼓楼医院", 118.789756, 32.063179, "img/s4.png", "src/4.jpg");
+landmarks[5] = new landmark(5, "赛珍珠纪念馆", 118.787487, 32.063273, "img/s5.png", "src/5.jpg");
+landmarks[6] = new landmark(6, "金陵大学", 118.784773, 32.061284, "img/s6.png", "src/6.jpg");
+landmarks[7] = new landmark(7, "金陵女子文理学院", 118.775830, 32.059739, "img/s7.png", "src/7.jpg");
+landmarks[8] = new landmark(8, "拉贝故居", 118.789863, 32.056754, "img/s8.png", "src/8.jpg");
+landmarks[9] = new landmark(9, "金陵神学院", 118.784569, 32.049938, "img/s9.png", "src/9.jpg");
 landmarks[1].modern_init();
 
 //紫金草
-var moveIcon = new BMapGL.Icon("img/icon.png", new BMapGL.Size(32, 32));
+var moveIcon = new BMapGL.Icon("img/icon.png", new BMapGL.Size(32, 60));
 var moveMarker = new BMapGL.Marker(landmarks[1].point, {icon: moveIcon});
 var pts = [[], [], [], [], [], [], [], []];//生成轨迹
 var state;//状态
@@ -97,12 +179,8 @@ landmark.prototype.walk = function(speed) {
         }
 
         setTimeout(function () {   
-            map.setDisplayOptions({
-                poiText: false,  // 隐藏poi标注
-                poiIcon: false,  // 隐藏poi图标
-            });    
             playvideo(index - 1, speed);
-        }, 1000);  
+        }, 0);  
     })
 };
 
@@ -117,48 +195,6 @@ function playvideo(index, speed) {
             ptss[num] = item;
         }
     });
-
-    /*let ptsss = [];
-    //alert(ptss.length);
-    for (i = 0; i < 35; i++) {
-        ptsss[i] = ptss[i];
-        //console.log(ptsss[i]);
-    }
-
-    let pl = new BMapGL.Polyline(ptsss);
-    let trackAni = new BMapGLLib.TrackAnimation(map, pl, {
-        overallView: true, // 动画完成后自动调整视野到总览
-        tilt: 45,          // 轨迹播放的角度，默认为55
-        duration: 500,   // 动画持续时长，默认为10000，单位ms
-        //delay: 3000        // 动画开始的延迟，默认0，单位ms
-    }); 
-    trackAni.start();   
-    
-    for (i = 0; i < 35; i++) {
-        ptsss[i] = ptss[35+i];
-    }
-
-    pl = new BMapGL.Polyline(ptsss);
-    trackAni = new BMapGLLib.TrackAnimation(map, pl, {
-        overallView: true, // 动画完成后自动调整视野到总览
-        tilt: 45,          // 轨迹播放的角度，默认为55
-        duration: 500,   // 动画持续时长，默认为10000，单位ms
-        delay: 501        // 动画开始的延迟，默认0，单位ms
-    }); 
-    trackAni.start();   
-
-    for (i = 0; i < 35; i++) {
-        ptsss[i] = ptss[75+i];
-    }
-
-    pl = new BMapGL.Polyline(ptsss);
-    trackAni = new BMapGLLib.TrackAnimation(map, pl, {
-        overallView: true, // 动画完成后自动调整视野到总览
-        tilt: 45,          // 轨迹播放的角度，默认为55
-        duration: 5000,   // 动画持续时长，默认为10000，单位ms
-        delay: 1002        // 动画开始的延迟，默认0，单位ms
-    }); 
-    trackAni.start();  */ 
     
     map.addOverlay(moveMarker);         
     var i = 0;         
@@ -185,11 +221,13 @@ function playvideo(index, speed) {
         } 
         else {
             map.removeOverlay(moveMarker);
-            map.setDisplayOptions({
-                poiText: true,  // 隐藏poi标注
-                poiIcon: true  // 隐藏poi图标
-            });
-            //console.log("im here");
+
+
+
+            //location.reload();
+
+
+
         }         
     }
     setTimeout(function () {         
@@ -198,6 +236,7 @@ function playvideo(index, speed) {
 }
 
 function init() {
+    //modern
     state = localStorage.getItem("state");
     if(state == null || state > 9) {
         state = 1;
@@ -212,4 +251,35 @@ function init() {
             finish();
         }
     }
+    map.centerAndZoom(landmarks[state].point, 16);
+
+    const file = new XMLHttpRequest();
+    file.open("GET", "questions.txt", true);
+    file.onload = function () {
+        const allText = file.response;
+        let texts = allText.split('\n');
+        for (i = 0; i < texts.length; i++) {
+            //console.log(texts[i]);
+            let text = texts[i].split(' ');
+            landmarks[text[0]].name = text[1];
+            landmarks[text[0]].questions.push([text[2], text[3]]);
+        }
+    }
+    file.send();
+    
+    const file_ = new XMLHttpRequest();
+    file_.open("GET", "introduction.txt", true);
+    file_.onload = function() {
+        const allText_ = file_.response;
+        let texts_ = allText_.split('\n');
+        for (i = 0; i < texts_.length; i++) {
+            let text_ = texts_[i].split(' ');
+            for(j = 1; j < text_.length; j++) {
+                landmarks[text_[0]].introduction_word.push(text_[j]);
+            }
+        }
+    }
+    file_.send();
+    
 };init();
+
