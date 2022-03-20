36d4b7a4cf25acf9915425145a42eb9b344a5443
CDD <70897880+CDDNB@users.noreply.github.com>
2022-03-21 03:45:25 +0800

    Add files via upload

--- modern.js
+++ modern.js
@@ -10,7 +10,9 @@ function username_submit() {
         document.getElementById("username").style.display = 'none';
         document.getElementById("userbutton").style.display = 'none';
         document.getElementById("all").style.left = "0";
-        map.setCenter(landmarks[state].point);
+        map.centerAndZoom(landmarks[state].point, 16);
+        //map.setCenter(landmarks[state].point);
+        make_certificate();
         //console.log(map.getCenter());
     }
 }
@@ -128,37 +130,52 @@ function answer_wrong(this_choice) {
 function finish() {
     book.play();
     //document.getElementById("modern").style.display = 'none';
-    var ctx = canvas.getContext("2d");
-    var img = new Image();
-    var Url;
-    img.setAttribute("crossOrigin", "anonymous");
-    img.src = "img/certificate.png";
-    img.onload = function() {
-        ctx.drawImage(img, 0, 0);
-        ctx.font = "100px Arial";
-        ctx.textAlign = 'center';
-        ctx.fillText(username, 1115, 1970);
-        var a = document.getElementById("a");
-        Url = canvas.toDataURL();
-        console.log(a.href);
-        a.setAttribute("href", Url);
-        console.log(a.href);
-    }
+    
     canvas.style.display = 'block';
     document.getElementById("save").style.display = 'block';
     canvas.addEventListener("click", function() {
         //canvas.style.display = 'none';
-        var blob = new Blob([''], { type: 'application/octet-stream' });
-		var url = URL.createObjectURL(blob);
-		var aa = document.createElement('aa');
-		aa.href = Url;
-		aa.download = Url.replace(/(.*\/)*([^.]+.*)/ig, "$2").split("?")[0];
-		var e = document.createEvent('MouseEvents');
-		e.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
-		aa.dispatchEvent(e);
-		URL.revokeObjectURL(url);
+        //document.getElementById("save").style.display = 'none';
+        console.log(url);
+        savePic(url);
     });
-}
+};
+
+
+
+function savePic(Url){
+    Url = this.dialogImgUrl //图片路径，也可以传值进来
+    var triggerEvent = "touchstart"; //指定下载方式
+    var blob=new Blob([''], {type:'application/octet-stream'}); //二进制大型对象blob
+    var url = URL.createObjectURL(blob); //创建一个字符串路径空位
+    var a = document.createElement('a'); //创建一个 a 标签
+    a.href = Url;  //把路径赋到a标签的href上
+    //正则表达式，这里是把图片文件名分离出来。拿到文件名赋到a.download,作为文件名来使用文本
+    a.download = "try_phone"; 
+    /* var e = document.createEvent('MouseEvents');  //创建事件（MouseEvents鼠标事件）
+    e.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null); //初始化鼠标事件（initMouseEvent已弃用）*/
+    
+    //代替方法。创建鼠标事件并初始化（后面这些参数我也不清楚，参考文档吧 https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/MouseEvent）
+    var e = new MouseEvent('click', ( true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null));  
+    //派遣后，它将不再执行任何操作。执行保存到本地
+    a.dispatchEvent(e); 
+    //释放一个已经存在的路径（有创建createObjectURL就要释放revokeObjectURL）
+    URL.revokeObjectURL(url);  
+    /* 
+    //这段好像并不影响，所以我不用，注释掉
+    var imgs = document.getElementsByTagName("img");
+    for(var i = 0,o;o = imgs[i];i++){
+        
+        o.addEventListener(triggerEvent,function(){
+            var newurl = this.getAttribute("src");
+            saveAs(newurl);
+        },false);
+    } */
+    
+};
+
+
+
 
 var landmarks = new Array();
 
@@ -331,6 +348,26 @@ function playvideo(index, speed) {
     }, speed);         
 }
 
+var url;
+
+function make_certificate() {
+    var ctx = canvas.getContext("2d");
+    var img = new Image();
+    img.setAttribute("crossOrigin", "anonymous");
+    img.src = "img/certificate.png";
+    img.onload = function() {
+        ctx.drawImage(img, 0, 0);
+        ctx.font = "100px Arial";
+        ctx.textAlign = 'center';
+        ctx.fillText(username, 1250, 2300);
+        //var a = document.getElementById("a");
+        url = canvas.toDataURL();
+        //console.log(a.href);
+        //a.setAttribute("href", url);
+        //console.log(a.href);
+    }
+}
+
 function init() {
     username = localStorage.getItem("name");
     if(username == null) {
@@ -339,6 +376,7 @@ function init() {
     else {
         document.getElementById("username").style.display = 'none';
         document.getElementById("userbutton").style.display = 'none';
+        make_certificate();
     }
 
     //modern
