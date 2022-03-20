8ac1401c9eb1d41cfc79b8784a0e84c8863dea50
CDD <70897880+CDDNB@users.noreply.github.com>
2022-03-21 03:50:01 +0800

    Add files via upload

--- modern.js
+++ modern.js
@@ -136,46 +136,9 @@ function finish() {
     canvas.addEventListener("click", function() {
         //canvas.style.display = 'none';
         //document.getElementById("save").style.display = 'none';
-        console.log(url);
-        savePic(url);
-    });
-};
-
-
-
-function savePic(Url){
-    Url = this.dialogImgUrl //图片路径，也可以传值进来
-    var triggerEvent = "touchstart"; //指定下载方式
-    var blob=new Blob([''], {type:'application/octet-stream'}); //二进制大型对象blob
-    var url = URL.createObjectURL(blob); //创建一个字符串路径空位
-    var a = document.createElement('a'); //创建一个 a 标签
-    a.href = Url;  //把路径赋到a标签的href上
-    //正则表达式，这里是把图片文件名分离出来。拿到文件名赋到a.download,作为文件名来使用文本
-    a.download = "try_phone"; 
-    /* var e = document.createEvent('MouseEvents');  //创建事件（MouseEvents鼠标事件）
-    e.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null); //初始化鼠标事件（initMouseEvent已弃用）*/
-    
-    //代替方法。创建鼠标事件并初始化（后面这些参数我也不清楚，参考文档吧 https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/MouseEvent）
-    var e = new MouseEvent('click', ( true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null));  
-    //派遣后，它将不再执行任何操作。执行保存到本地
-    a.dispatchEvent(e); 
-    //释放一个已经存在的路径（有创建createObjectURL就要释放revokeObjectURL）
-    URL.revokeObjectURL(url);  
-    /* 
-    //这段好像并不影响，所以我不用，注释掉
-    var imgs = document.getElementsByTagName("img");
-    for(var i = 0,o;o = imgs[i];i++){
         
-        o.addEventListener(triggerEvent,function(){
-            var newurl = this.getAttribute("src");
-            saveAs(newurl);
-        },false);
-    } */
-    
-};
-
-
-
+    });
+}
 
 var landmarks = new Array();
 
@@ -348,8 +311,6 @@ function playvideo(index, speed) {
     }, speed);         
 }
 
-var url;
-
 function make_certificate() {
     var ctx = canvas.getContext("2d");
     var img = new Image();
@@ -360,10 +321,10 @@ function make_certificate() {
         ctx.font = "100px Arial";
         ctx.textAlign = 'center';
         ctx.fillText(username, 1250, 2300);
-        //var a = document.getElementById("a");
-        url = canvas.toDataURL();
+        var a = document.getElementById("a");
+        let url = canvas.toDataURL();
         //console.log(a.href);
-        //a.setAttribute("href", url);
+        a.setAttribute("href", url);
         //console.log(a.href);
     }
 }
